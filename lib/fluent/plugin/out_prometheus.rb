d590b57b11bd69d9efef7e5aee9e814879628fc5
KIM BYOUNG GON <fharenheit@gmail.com>
2025-01-11 12:04:54 +0900

    로직 및 테스트 코드 수정

--- lib/fluent/plugin/out_prometheus.rb
+++ lib/fluent/plugin/out_prometheus.rb
@@ -4,31 +4,45 @@ require 'prometheus/client'
 require 'prometheus/client/push'
 require 'net/http'
 require 'uri'
+require 'socket'
+
+########################################
+## 이 테스트 코드를 윈도에서 실행하기 위해서는
+# TZ 환경변수에 Asia/Seoul을 설정합니다.
+########################################
 
 module Fluent
   module Plugin
     class PrometheusOut < Fluent::Plugin::Output
       Fluent::Plugin.register_output("prometheus-pushgateway", self)
 
-      attr_accessor :engine, :scheduler, :registry, :gateway, :job
+      attr_accessor :engine, :scheduler, :registry, :gateway_server, :job, :gateway, :instance
 
       def configure(conf)
         super
-        $log.info "Prometheus Output Configure Started\n"
+        $log.info "Prometheus Output Configure Started"
 
-        @gateway = conf["gateway"]
+        @instance = conf["hostname"] || Socket.gethostname
+        @gateway_server = conf["gateway"]
         @job = conf["job"]
 
-        $log.info "Prometheus Push Gateway: #{@gateway}\n"
-        $log.info "Prometheus Job: #{job}\n"
+        $log.info "Prometheus Push Gateway Configuration : #{conf}"
+
+        @registry = Prometheus::Client.registry
+        @gateway = Prometheus::Client::Push.new(job: @job, gateway: "http://#{@gateway_server}")
+
+        $log.info "Prometheus Push Gateway: #{@gateway_server}"
+        $log.info "Prometheus Job: #{@job}"
+        $log.info "Prometheus Instance: #{@instance}"
+        $log.info "Prometheus Push Gateway Connector Created"
 
         @scheduler = Rufus::Scheduler.new
-        @registry = Prometheus::Client.registry
+        $log.info "Rufus Scheduler Started"
 
         @scheduler.every '15s' do
-          push_to_gateway(generate_metrics_text)
+          @gateway.add(registry)
 
-          $log.info "Pushing to Prometheus Push Gateway: #{@gateway}"
+          $log.info "Pushing to Prometheus Push Gateway: http://#{@gateway_server}"
         end
       end
 
@@ -46,13 +60,12 @@ module Fluent
         # 메시지가 유입되면 Prometheus Metric을 생성하고 처리한다.
         # 대부분은 Counter이므로 increment 처리를 하지만 Gauge는 값을 Setting해야 한다.
         records.each do |time, record|
-          labels = { type: record["type"], instance: record["instance"], source: record["job"] }
           if @registry.get(record["metric-name"]).nil?
-            @registry.counter(to_symbol(record["metric-name"]), docstring: record["metric-name"], labels: [:type, :instance, :source])
+            @registry.counter(to_symbol(record["metric-name"]), docstring: record["metric-desc"], labels: [:type, :instance, :source])
           end
 
           m = @registry.get(record["metric-name"])
-          m.increment(labels: labels)
+          m.increment(labels: { type: record["type"], instance: @instance, source: @job })
 
           $log.debug "Prometheus Metric: #{m.inspect}"
         end
@@ -60,19 +73,6 @@ module Fluent
 
       def generate_metrics_text
         Prometheus::Client::Formats::Text.marshal(Prometheus::Client.registry)
-      end
-
-      def push_to_gateway(metrics_text)
-        uri = URI.parse("http://#{@gateway}/metrics/job/#{@job}")
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Put.new(uri.path)
-        request.body = metrics_text
-        request['Content-Type'] = 'text/plain'
-
-        response = http.request(request)
-        $log.debug "Response from Push Gateway: #{response.code} #{response.message}"
-      rescue StandardError => e
-        $log.warn "Failed to push metrics to Push Gateway: #{e.message}"
       end
     end
   end
--- test/plugin/test_out_prometheus.rb
+++ test/plugin/test_out_prometheus.rb
@@ -1,31 +1,64 @@
 require "helper"
 require "fluent/plugin/out_prometheus.rb"
+require 'net/http'
+require 'uri'
 
 class PrometheusOutTest < Test::Unit::TestCase
   setup do
     Fluent::Test.setup
   end
 
-  CONFIG = %[
-    gateway localhost:9091
-    job impala-kudu-metrics
-  ]
+  CONFIG = { "gateway" => "localhost:9091", "job" => "impala-kudu-metrics" }
 
   messages = [
-    { "message" => "Unable to open scanner for node with id ‘0’ for Kudu table ‘nc.d_so’ : Time out : exceeded configure scan timeout of 180.000s: Scan RPC to x.x.x.x:7050 timed out after 179.999 (ON_OUTBOUND_QUEUE)", "category" => "prometheus", "type" => "SCAN_TIMEOUT", "metric-type" => "Counter", "metric-name" => "kudu_scan_timeout_count", "table_name" => "nc.d_so", "job" => "fluentd-plugin-kudu", "instance" => "mbp14pro.local" },
-    { "message" => "Row Projector request submit failed: service unavailable: Thread pool is at capacity(1/1 tasks running , 100/100 tasks queued) [suppressed 79 similar messages]Proxy.cc:231] call had error, refreshing address and retrying: Remote error: Service unavailable: Update Consensus request on kudu.consensus. consensusService from 1.1.1.1:55108 dropped due to backpressure. The service queue is full; it has 50 items", "category" => "prometheus", "type" => "BACKPRESSURE", "metric-type" => "Counter", "metric-name" => "kudu_backpressure_count", "items" => "50", "current_running_tasks" => 1, "max_running_tasks" => 1, "current_queued_tasks" => 100, "max_queued_tasks" => 100, "job" => "fluentd-plugin-kudu", "instance" => "mbp14pro.local" },
-    { "message" => "Failed to write batch of 6 ops to tablet after 1 attempt : Failed to write to server : Write RPC to  x.x.x.x:7050 timed out after 179.999 (ON_OUTBOUND_QUEUE)", "category" => "prometheus", "type" => "WRITER_TIMEOUT", "metric-type" => "Counter", "metric-name" => "kudu_write_batch_timeout_count", "job" => "fluentd-plugin-kudu", "instance" => "mbp14pro.local" }
+    {
+      "message" => "Unable to open scanner for node with id ‘0’ for Kudu table ‘nc.d_so’ : Time out : exceeded configure scan timeout of 180.000s: Scan RPC to x.x.x.x:7050 timed out after 179.999 (ON_OUTBOUND_QUEUE)",
+      "category" => "prometheus",
+      "type" => "SCAN_TIMEOUT",
+      "metric-type" => "Counter",
+      "metric-name" => "kudu_scan_timeout_count",
+      "table_name" => "nc.d_so",
+      "metric-desc" => "Kudu Scan Timeout 건수"
+    },
+    {
+      "message" => "Row Projector request submit failed: service unavailable: Thread pool is at capacity(1/1 tasks running , 100/100 tasks queued) [suppressed 79 similar messages]Proxy.cc:231] call had error, refreshing address and retrying: Remote error: Service unavailable: Update Consensus request on kudu.consensus. consensusService from 1.1.1.1:55108 dropped due to backpressure. The service queue is full; it has 50 items",
+      "category" => "prometheus",
+      "type" => "BACKPRESSURE",
+      "metric-type" => "Counter",
+      "metric-name" => "kudu_backpressure_count",
+      "items" => "50",
+      "current_running_tasks" => 1,
+      "max_running_tasks" => 1,
+      "current_queued_tasks" => 100,
+      "max_queued_tasks" => 100,
+      "metric-desc" => "Kudu Backpressure 건수"
+    },
+    {
+      "message" => "Failed to write batch of 6 ops to tablet after 1 attempt : Failed to write to server : Write RPC to  x.x.x.x:7050 timed out after 179.999 (ON_OUTBOUND_QUEUE)",
+      "category" => "prometheus",
+      "type" => "WRITER_TIMEOUT",
+      "metric-type" => "Counter",
+      "metric-name" => "kudu_write_batch_timeout_count",
+      "metric-desc" => "Kudu Client Timeout 건수"
+    }
   ]
 
   test "messages1_test" do
-    driver = create_driver(CONFIG)
+    # 단위 테스트를 위해서 create_driver로 전달하기 위해서 Hash를 String으로 변환한다.
+    conf = CONFIG.map { |key, value| "#{key} #{value}" }.join("\n")
+    puts "Driver Configuration : \n#{conf}"
+
+    driver = create_driver(conf)
     driver.run(default_tag: "test") do
       messages.each do |message|
         driver.feed(message) # 로그 메시지를 주입
       end
     end
 
-    puts Prometheus::Client::Formats::Text.marshal(Prometheus::Client.registry)
+    registry = Prometheus::Client.registry
+    gateway = Prometheus::Client::Push.new(job: CONFIG["job"], gateway: "http://#{CONFIG["gateway"]}")
+
+    gateway.add(registry)
   end
 
   private
